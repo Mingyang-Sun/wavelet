a19f3a87f3536dc032e1421b5489d5b0275f46d4
Mingyang-Sun <mingyangs0301@gmail.com>
2022-10-14 18:43:49 -0700

    done

--- SearchEngine.java
+++ SearchEngine.java
@@ -2,10 +2,12 @@ import java.io.IOException;
 import java.net.URI;
 import java.util.ArrayList;
 
-class Handler implements URLHandler {
+class Handler implements URLHandler
+{
     ArrayList<String> list = new ArrayList<>();
 
-    public String handleRequest(URI url) {
+    public String handleRequest(URI url)
+    {
         if (url.getPath().equals("/"))
         {
             return "No action";
@@ -44,15 +46,16 @@ class Handler implements URLHandler {
     }
 }
 
-class SearchEngine {
-    public static void main(String[] args) throws IOException {
-        if(args.length == 0){
+class SearchEngine
+{
+    public static void main(String[] args) throws IOException
+    {
+        if(args.length == 0)
+        {
             System.out.println("Missing port number! Try any number between 1024 to 49151");
             return;
         }
-
         int port = Integer.parseInt(args[0]);
-
         Server.start(port, new Handler());
     }
 }
